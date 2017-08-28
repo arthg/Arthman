a18cf9f95fccf510ac1b5d20ad39af2a55bb7a6e
Arthur Gorr <art.gorr@gmail.com>
2017-08-28 07:18:53 -0500

    NancyFx, AspNetCore.Owinn packages Upgrade to ASP.NET Core 2.0 Hello World

---
+++ Arthman/api/Index/IndexModule.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Arthman.api.Index
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get("/", args => "Hello World");
+        }
+    }
+}
