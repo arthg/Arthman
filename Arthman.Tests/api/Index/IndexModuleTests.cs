83a4d580cee6a123a49a2a67da54c3b86f93e63e
Arthur Gorr <art.gorr@gmail.com>
2017-08-28 22:11:00 -0500

    Fixed up references.  Don't know who that happened - test project not in expceted folder location.

---
+++ Arthman.Tests/api/Index/IndexModuleTests.cs
@@ -0,0 +1,37 @@
+﻿using Nancy;
+using Nancy.Testing;
+using NUnit.Framework;
+using SharpTestsEx;
+using System.Threading.Tasks;
+using Arthman.Tests.helpers;
+
+namespace Arthman.Tests.api.Index
+{
+    [TestFixture]
+    public abstract class IndexModuleTests
+    {
+        private Browser _browser;
+
+        [SetUp]
+        public void PrepareTestBrowser()
+        {
+            var defaultNancyBootstrapper = new DefaultNancyBootstrapper();
+            _browser = new Browser(defaultNancyBootstrapper);
+        }
+
+        public sealed class IndexGetEndpoint : IndexModuleTests
+        {
+            [Test]
+            public async Task Should_return_statuc_code_ok_Async()
+            {
+                //arrange
+
+                //act
+                var result = await _browser.Get("/");
+
+                //assert
+                result.StatusCode.Should().Be(HttpStatusCode.OK);
+            }
+        }
+    }
+}
---
+++ Arthman.Tests/helpers/TestBrowserExtensions.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Nancy.Testing;
+using Newtonsoft.Json;
+
+namespace Arthman.Tests.helpers
+{
+    public static class TestBrowserExtensions
+    {
+        public static TModel BodyAsJson<TModel>(this BrowserResponse response)
+        {
+            if (!response.ContentType.Contains("application/json"))
+            {
+                throw new ArgumentException("Not json body.");
+            }
+            var json = response.Body.AsString();
+            var settings = new JsonSerializerSettings();
+            return JsonConvert.DeserializeObject<TModel>(json, settings);
+        }
+
+        public static dynamic BodyAsDynamic(this BrowserResponse response)
+        {
+            return response.BodyAsJson<dynamic>();
+        }
+
+        public static byte[] BodyAsBytes(this BrowserResponse response)
+        {
+            var responseStream = new MemoryStream();
+            response.Context.Response.Contents(responseStream);
+            return responseStream.ToArray();
+        }
+
+        public static BrowserContext QueryString(this BrowserContext context, string key, string value)
+        {
+            context.Query(key, value);
+            return context;
+        }
+
+        public static BrowserContext Query(this BrowserContext context, IEnumerable<KeyValuePair<string, string>> queryValues)
+        {
+            foreach (var queryValue in queryValues)
+            {
+                context.Query(queryValue.Key, queryValue.Value);
+            }
+            return context;
+        }
+
+        public static BrowserContext HttpsBrowserContext(this BrowserContext context)
+        {
+            context.HttpsRequest();
+            return context;
+        }
+
+        public static BrowserContext JsonBrowserContext(this BrowserContext context, object request)
+        {
+            context.Body(JsonConvert.SerializeObject(request));
+            context.Header("Content-Type", "application/json");
+            return context;
+        }
+
+        public static BrowserContext ApiAuthToken(this BrowserContext context, string token)
+        {
+            context.Header("Authorization", "Token " + token);
+            return context;
+        }
+    }
+}
